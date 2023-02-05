bc563f59c893e4a2ad856f85d27936997578b41f
Shubham Joshi <subhamjoshi1998@gmail.com>
2023-02-05 19:42:15 +0530

    html

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const form = document.querySelector('#income-dettail-form')
 
+const taxableIncomeValue = document.querySelector('.taxable-income .value');
+const incomeTaxAfterValue = document.querySelector('.Income-tax-after .value');
+const healthCssValue = document.querySelector('.health-css .value');
+const netTaxValue = document.querySelector('.Net-tax .value');
+
 function calclculatorIncomeTax(income){
     let tax= 0;
     if(income <=700000){
@@ -8,31 +13,48 @@ function calclculatorIncomeTax(income){
         income = income - 50000
         tax = (300000) * 0.05 + (income - 600000) * 0.10
         health = (tax) * 0.04
-        tax = tax + health
+        netTax = tax + health
     }else if(income > 900000 && income <= 1200000){
         income = income - 50000
         tax = (300000) * 0.05 + (300000) * 0.10 + (income - 900000) * 0.15
         health = (tax) * 0.04
-        tax = tax + health
+        netTax = tax + health
     }else if(income > 1200000 && income <= 1500000){
         income = income - 50000
         tax = (300000) * 0.05 + (300000) * 0.10 + ( 300000) * 0.15 + (income - 1200000) * 0.20
         health = (tax) * 0.04
-        tax = tax + health
+        netTax = tax + health
     }else{
         income = income - 50000
         tax = (300000 * 0.05) + (300000 * 0.10) +  (300000 * 0.15) + (300000 * 0.20) + (income - 1500000) * 0.30
         health = (tax) * 0.04
-        tax = tax + health
+        netTax = tax + health
     }
-    return tax;
+    return netTax;
+}
+
+const updateData = (netTax) => {
+    taxableIncomeValue.innerHTML = Math.round(income);
+
+    let incomeTax = Math.round(tax);
+    incomeTaxAfterValue.innerHTML = incomeTax;
+
+    let healthCss = Math.round(health);
+    healthCssValue.innerHTML = healthCss;
+
+    let netIncomeTax = Math.round(netTax);
+    netTaxValue.innerHTML = netIncomeTax;
 }
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
 
     const income = Number(form.income.value);
-    const tax = calclculatorIncomeTax(income);
+    const netTax = calclculatorIncomeTax(income);
 
-    alert(`The income tax of your Annual Income is: ${tax}`);
+    alert(`The income tax of your Annual Income is: ${netTax}`);
 })
+
+const init = () => {
+    let netTax = updateData(netTax);
+};
